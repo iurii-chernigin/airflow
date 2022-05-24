a88536ada1ef881f4ecd0716593a1f5ca0850e18
iurii-chernigin <chernigin.y@gmail.com>
2022-05-24 10:21:46 +0300

    Init rickmorty_top_locations DAG

--- dags/rickmorty/rickmorty_top_locations.py
+++ dags/rickmorty/rickmorty_top_locations.py
@@ -12,3 +12,20 @@
     TODO: The DAG should be run every day
     TODO: The tasks should be restarted if it failed
 """
+from airflow.decorators import dag, task
+from airflow.operators.python import get_current_context
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'schedule_interval': '@daily'
+}
+
+@dag(
+    max_active_runs=1,
+    catchup=False,
+    tags=['rickmorty', 'rickmorty-top-locations', 'rickmorty-api-analyze'],
+    default_args=DEFAULT_ARGS
+) 
+def rickmorty_top_locations():
+    pass
